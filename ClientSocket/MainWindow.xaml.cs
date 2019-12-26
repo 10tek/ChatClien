0f696ea519f5c0c31ad5d217c34f821743c897fd
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-12-26 18:35:02 +0600

    Add project files.

---
+++ ClientSocket/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientSocket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var localIp = IPAddress.Parse("127.0.0.1");
+                var port = 8080;
+                var endPoint = new IPEndPoint(localIp, port);
+
+                socket.Connect(endPoint);
+
+                var mes =$"{userTB.Text}|userMessage|{textTB.Text}";
+                var buffer = Encoding.UTF8.GetBytes(mes);
+
+                socket.Send(buffer);
+                socket.Shutdown(SocketShutdown.Both);
+            }
+        }
+    }
+}
