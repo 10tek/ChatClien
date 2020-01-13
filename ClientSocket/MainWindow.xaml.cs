404e41f6bd8fbfd6c5b58c6b6ab5d4613304f58c
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-01-13 17:54:08 +0600

    Мелкие правки

--- ClientSocket/MainWindow.xaml.cs
+++ ClientSocket/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace ClientSocket
 {
@@ -22,27 +23,54 @@ namespace ClientSocket
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Socket socket;
+
         public MainWindow()
         {
             InitializeComponent();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localIp = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+            var endPoint = new IPEndPoint(localIp, port);
+
+            socket.Connect(endPoint);
         }
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
+        private async void ButtonClick(object sender, RoutedEventArgs e)
         {
-            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            var mes = new Message
             {
-                var localIp = IPAddress.Parse("127.0.0.1");
-                var port = 8080;
-                var endPoint = new IPEndPoint(localIp, port);
+                User = userTB.Text,
+                Text = textTB.Text
+            };
 
-                socket.Connect(endPoint);
+            var jsonMes = JsonConvert.SerializeObject(mes);
 
-                var mes =$"{userTB.Text}|userMessage|{textTB.Text}";
-                var buffer = Encoding.UTF8.GetBytes(mes);
+            var buffer = Encoding.UTF8.GetBytes(jsonMes);
+            ArraySegment<byte> data = new ArraySegment<byte>(buffer);
 
-                socket.Send(buffer);
-                socket.Shutdown(SocketShutdown.Both);
+            await socket.SendAsync(data, SocketFlags.None);
+
+            await GetMessages();
+        }
+
+        private async Task GetMessages()
+        {
+            messageHistory.Content = string.Empty;
+            var stringBuilder = new StringBuilder();
+            while (socket.Available > 0)
+            {
+                var buffer = new byte[1024];
+                ArraySegment<byte> data = new ArraySegment<byte>(buffer);
+                await socket.ReceiveAsync(data, SocketFlags.None);
+                stringBuilder.Append(Encoding.UTF8.GetString(buffer));
             }
+            var messages = JsonConvert.DeserializeObject<List<Message>>(stringBuilder.ToString());
+            foreach(var mes in messages)
+            {
+                messageHistory.Content += mes.ToString();
+            }
+            
         }
     }
 }
---
+++ ClientSocket/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+    public class Message
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public string User { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"  {User} :  {Text}    : {CreationDate}";
+        }
+    }
+}
